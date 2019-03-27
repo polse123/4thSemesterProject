fa830e683a2015d5374de44d13de9ca310422a00
niko6116 <niko6116@github.com>
2019-03-27 13:00:58 +0100

    Login view HTML

--- SCAMS/SCAMS/Controllers/HomeController.cs
+++ SCAMS/SCAMS/Controllers/HomeController.cs
@@ -1,37 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SCAMS.Models;
+using System.Diagnostics;
 
-namespace SCAMS.Controllers {
-    public class HomeController : Controller {
-        public IActionResult Index() {
+namespace SCAMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
             return View();
         }
 
-        //Comment by brchr.
-        //comment by thorey
-        public IActionResult About() {
+        public IActionResult About()
+        {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact() {
+        public IActionResult Contact()
+        {
             ViewData["Message"] = "Your contact page.";
 
             return View();
         }
 
-        public IActionResult Privacy() {
+        public IActionResult Privacy()
+        {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
+        public IActionResult Error()
+        {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
