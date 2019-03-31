b3cc777e3fff9c5c96f1e4462e1c6987f5ab1c25
0xjb <Jonas_46@msn.com>
2019-03-31 20:48:17 +0200

    Fix part 2

---
+++ ProjectSCAM/ProjectSCAM/Controllers/HistoryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SCAMS.Controllers
+{
+    public class HistoryController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectSCAM/ProjectSCAM/Controllers/OverviewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using SCAMS.Models;
+
+namespace SCAMS.Controllers {
+    public class OverviewController : Controller {
+        public object OpcClientModel { get; private set; }
+
+        public ActionResult Index() {
+            return View();
+        }
+        //not being used rn
+        //public void Setup(int amountToProduce, int speed, int productType) {
+        //    opc = new OPCMachine(amountToProduce);
+        //    opc.Speed = speed;
+        //    opc.ProductType = productType;
+        //}
+        public void Message() {
+            Response.ContentType = "text/event-stream";
+            OpcClient opc = new OpcClient();
+            do {
+                Response.Write("data:" + JsonConvert.SerializeObject(opc, Formatting.None) + "\n\n");
+                Response.FlushAsync();
+                Thread.Sleep(1000);
+            } while (true);
+        }
+    }
+}
