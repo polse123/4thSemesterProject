82cf2223a98e44e5f2467463ac48a3efdf1bf722
Jonas Bjørstorp <jonas_46@msn.com>
2019-03-31 22:33:50 +0200

    Create empty singleton

---
+++ ProjectSCAM/ProjectSCAM/Models/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSCAM.Models {
+    public sealed class Singleton {
+        // should contain opc & db modules
+        // e.g OPCManager & DBManager
+        // all logic & persistence belongs to the model layer
+        private static readonly Singleton instance = new Singleton();
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static Singleton() {
+        }
+
+        private Singleton() {
+        }
+
+        public static Singleton Instance {
+            get {
+                return instance;
+            }
+        }
+    }
+}
