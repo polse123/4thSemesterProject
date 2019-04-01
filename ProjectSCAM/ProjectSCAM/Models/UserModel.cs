def8fba92e6d39bdc5a099e46e75cd6a487bec4e
0xjb <Jonas_46@msn.com>
2019-04-01 12:10:00 +0200

    Create models & DBManager

---
+++ ProjectSCAM/ProjectSCAM/Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSCAM.Models {
+    public class UserModel {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+        public int UserType { get; set; }
+
+        public UserModel(int id, string username, string password, string firstName, string lastName, string email, string phoneNumber, bool isActive, int userType) {
+            Id = id;
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+            Password = password ?? throw new ArgumentNullException(nameof(password));
+            FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+            Email = email ?? throw new ArgumentNullException(nameof(email));
+            PhoneNumber = phoneNumber;
+            IsActive = isActive;
+            UserType = userType;
+        }
+    }
+}
