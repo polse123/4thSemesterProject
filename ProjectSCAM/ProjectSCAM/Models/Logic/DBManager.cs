c84c331a38b22112209784b33c28cd45989ae791
Nikolaj Filipsen <niko6116@gmail.com>
2019-04-02 11:28:39 +0200

    small stuff

--- ProjectSCAM/ProjectSCAM/Models/Logic/DBManager.cs
+++ ProjectSCAM/ProjectSCAM/Models/Logic/DBManager.cs
@@ -4,12 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ProjectSCAM.Models.Logic {
-    public class DBManager {
+namespace ProjectSCAM.Models.Logic
+{
+    public class DBManager
+    {
         NpgsqlConnection conn;
-        public DBManager(string server, string port, string userid, string password, string database) {
-            initConnection(server, port, userid, password, database);
+
+        public DBManager(string server, string port, string userid, string password, string database)
+        {
+            InitConnection(server, port, userid, password, database);
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -18,41 +23,43 @@ namespace ProjectSCAM.Models.Logic {
         /// <param name="userid"></param>
         /// <param name="password"></param>
         /// <param name="database"></param>
-        private void initConnection(string server, string port, string userid,
-            string password, string database) {
+        private void InitConnection(string server, string port, string userid, string password, string database)
+        {
             // PostgeSQL-style connection string
             string connstring = String.Format("Server={0};Port={1};" +
                 "User Id={2};Password={3};Database={4};",
-                server, port, userid,
-                password, database);
+                server, port, userid, password, database);
             // Making connection with Npgsql provider
-            conn= new NpgsqlConnection(connstring);
-        }   
-        public LinkedList<UserModel> retrieveUsers() {
+            conn = new NpgsqlConnection(connstring);
+        }
+
+        public LinkedList<UserModel> RetrieveUsers()
+        {
             LinkedList<UserModel> users = new LinkedList<UserModel>();
-            // change to async when using real database
-            try {
+            string query = "SELECT * FROM Users WHERE isactive = true";
+            try
+            {
                 conn.Open();
-                string query = "SELECT * FROM Users WHERE isactive = true";
                 NpgsqlCommand command = new NpgsqlCommand(query, conn);
                 NpgsqlDataReader dr = command.ExecuteReader();
-                while (dr.Read()) {
+                while (dr.Read())
+                {
                     UserModel user = new UserModel(
                         (int)dr[0], dr[1].ToString(), dr[2].ToString(), dr[3].ToString(),
                         dr[4].ToString(), dr[5].ToString(), dr[6].ToString(), (bool)dr[7], (int)dr[8]);
                     users.AddLast(user);
                 }
             }
-            catch(NpgsqlException ex) {
+            catch (NpgsqlException ex)
+            {
                 throw ex;
             }
-            finally {
+            finally
+            {
                 conn.Close();
             }
-
-
             return users;
-            
+
         }
     }
 }
