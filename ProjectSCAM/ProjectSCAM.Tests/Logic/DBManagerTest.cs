178d938458c35c9aabfdb944513b497b8effe60c
Nikolaj Filipsen <niko6116@gmail.com>
2019-04-25 14:17:01 +0200

    DBManagerTest now working

--- ProjectSCAM/ProjectSCAM.Tests/Logic/DBManagerTest.cs
+++ ProjectSCAM/ProjectSCAM.Tests/Logic/DBManagerTest.cs
@@ -1,28 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectSCAM;
 using ProjectSCAM.Models;
 using ProjectSCAM.Models.Logic;
 
 namespace ProjectSCAM.Tests.Logic
 {
     [TestClass]
-    class DBManagerTest
+    public class DBManagerTest
     {
+        public DBManagerTest()
+        {
+            dbManager = new DBManager(DB_INFO[0], DB_INFO[1], DB_INFO[2], DB_INFO[3], DB_INFO[4]);
+        }
+
         private DBManager dbManager;
 
         // server, port, user id, password, database
         private readonly string[] DB_INFO = {"balarama.db.elephantsql.com",
             "5432", "ppcrexqw", "HL8HORvW5RUPUlBUcf_PIcZWxjlOoc1F", "ppcrexqw"};
-
-        public DBManagerTest()
-        {
-            dbManager = new DBManager(DB_INFO[0], DB_INFO[1], DB_INFO[2], DB_INFO[3], DB_INFO[4]);
-        }
 
         [TestMethod]
         public void RetrieveUsertypes()
@@ -328,3 +324,6 @@ namespace ProjectSCAM.Tests.Logic
         }
     }
 }
+
+
+
