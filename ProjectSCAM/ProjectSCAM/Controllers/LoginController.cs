ff868aae973259771a769415a99c2a6e583bd324
Brian <brianchr4@gmail.com>
2019-05-07 14:46:14 +0200

    Authorize attempt

--- ProjectSCAM/ProjectSCAM/Controllers/LoginController.cs
+++ ProjectSCAM/ProjectSCAM/Controllers/LoginController.cs
@@ -1,8 +1,10 @@
-﻿using ProjectSCAM.Models;
+﻿using Microsoft.AspNet.Identity;
+using ProjectSCAM.Models;
 using ProjectSCAM.Models.Logic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -12,6 +14,7 @@ namespace SCAMS.Controllers
     {
         public ActionResult Index()
         {
+            System.Web.HttpContext.Current.Session["UserType"] = "hello";
             if (TempData["statusMessage"] != null)
             {
                 ViewBag.statusMessage = TempData["statusMessage"].ToString();
@@ -26,6 +29,7 @@ namespace SCAMS.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Login(LoginModel u)
         {
+            
             if (ModelState.IsValid)
             {
                 UserModel uM = Singleton.Instance.DBManager.RetrieveUser(u.Username, u.Password, true);
@@ -33,18 +37,24 @@ namespace SCAMS.Controllers
                 {
                     if (uM.UserType == 1)
                     {// session husk!!
+                        //Session["userType"] = uM.UserType;
+                        //model.userRole = usermodelDB.userRole;
+                        //FormsAuthentication.SetAuthCookie(model.userRole, true);
+                        //System.Web.HttpContext.Current.Session["UserRole"] = usermodelDB.userRole;
+                        //var ia = System.Web.HttpContext.Current.User.Identity.IsAuthenticated;
+                        System.Web.HttpContext.Current.Session["UserType"] = "1";
                         return RedirectToAction("Index", "Overview");
                     }
                 }
                 else
                 {   ViewBag.statusMessage = "Login failed";
-                    TempData["statusMessage"] = "Login failed";
+                    TempData["statusMessage"] = "Login failed 1";
                     return RedirectToAction("Index", new { Message = "Login failed" });
 
                 }
             }
             ViewBag.statusMessage = "Login failed";
-            TempData["statusMessage"] = "Login failed";
+            TempData["statusMessage"] = "Login failed 2";
             return RedirectToAction("Index", new { Message = "Login failed" });
 
         }
